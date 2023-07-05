0c87af1617a3c6280639027da05b99aab202b1c2
Ekaterina Bruch <ekaterinabruch@gmail.com>
2023-07-05 14:16:44 +0200

    Add interval

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -1,15 +1,32 @@
 import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
+async function fetcher(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
 export default function ISSTracker() {
+  const { data, error, isLoading, mutate } = useSWR(URL, fetcher, {
+    refreshInterval: 5000,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  /*
   const [coords, setCoords] = useState({
     longitude: 0,
     latitude: 0,
-  });
+  });*/
 
+  /*
   async function getISSCoords() {
     try {
       const response = await fetch(URL);
@@ -31,14 +48,14 @@ export default function ISSTracker() {
       clearInterval(timer);
     };
   }, []);
-
+*/
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Map longitude={data.longitude} latitude={data.latitude} />
       <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        longitude={data.longitude}
+        latitude={data.latitude}
+        onRefresh={() => mutate()}
       />
     </main>
   );
